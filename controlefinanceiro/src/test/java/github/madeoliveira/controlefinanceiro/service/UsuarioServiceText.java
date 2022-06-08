afbd97a766db6e7ad8c0f169ea5056eb7610b8f2
Your Name <you@example.com>
2022-06-08 19:57:51 -0300

    Adicionando Mocks nos testes de UsuarioService.

--- controlefinanceiro/src/test/java/github/madeoliveira/controlefinanceiro/service/UsuarioServiceText.java
+++ controlefinanceiro/src/test/java/github/madeoliveira/controlefinanceiro/service/UsuarioServiceText.java
@@ -1,7 +1,9 @@
 package github.madeoliveira.controlefinanceiro.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,26 +12,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 import github.madeoliveira.controlefinanceiro.exceptions.RegraNegocioException;
 import github.madeoliveira.controlefinanceiro.model.entity.Usuario;
 import github.madeoliveira.controlefinanceiro.model.repository.UsuarioRepository;
+import github.madeoliveira.controlefinanceiro.service.impl.UsuarioServiceImpl;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceText {
 
-	@Autowired
 	UsuarioService service;
-	@Autowired
 	UsuarioRepository repository;
+	
+	@Before
+	public void Setup() {
+		repository=Mockito.mock(UsuarioRepository.class);
+		service=new UsuarioServiceImpl(repository);
+	}
 
 	@Test(expected = Test.None.class)
 	public void deveValidarEmail() {
-		repository.deleteAll();
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 		service.validarEmail("email@email.com");
 	}
 	@Test(expected = RegraNegocioException.class)
-	public void deveLancarErrroAoValidarEmailQuandoExistirEmailCadastrado () {
-		Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-		repository.save(usuario);
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado () {
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		service.validarEmail("email@email.com");
 		
 	}
