33497b4f887c8219bc2ad613718ade0cd317f1f2
Your Name <you@example.com>
2022-06-08 20:08:05 -0300

    Criada uma instância do mock e add dentro do contexto spring.

--- controlefinanceiro/src/test/java/github/madeoliveira/controlefinanceiro/service/UsuarioServiceText.java
+++ controlefinanceiro/src/test/java/github/madeoliveira/controlefinanceiro/service/UsuarioServiceText.java
@@ -4,28 +4,25 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import github.madeoliveira.controlefinanceiro.exceptions.RegraNegocioException;
-import github.madeoliveira.controlefinanceiro.model.entity.Usuario;
 import github.madeoliveira.controlefinanceiro.model.repository.UsuarioRepository;
 import github.madeoliveira.controlefinanceiro.service.impl.UsuarioServiceImpl;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceText {
 
 	UsuarioService service;
+	@MockBean
 	UsuarioRepository repository;
-	
+
 	@Before
 	public void Setup() {
-		repository=Mockito.mock(UsuarioRepository.class);
-		service=new UsuarioServiceImpl(repository);
+		service = new UsuarioServiceImpl(repository);
 	}
 
 	@Test(expected = Test.None.class)
@@ -33,10 +30,11 @@ public class UsuarioServiceText {
 		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 		service.validarEmail("email@email.com");
 	}
+
 	@Test(expected = RegraNegocioException.class)
-	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado () {
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
 		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		service.validarEmail("email@email.com");
-		
+
 	}
 }
