aaca220ed0fea8c1315ab9cf7e2eaeb7459e5095
Your Name <you@example.com>
2022-06-10 11:30:42 -0300

    Implementado o EndPoint de autenticação de Usuários.

--- controlefinanceiro/src/main/java/github/madeoliveira/controlefinanceiro/api/resource/UsuarioResource.java
+++ controlefinanceiro/src/main/java/github/madeoliveira/controlefinanceiro/api/resource/UsuarioResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import github.madeoliveira.controlefinanceiro.api.dto.UsuarioDTO;
+import github.madeoliveira.controlefinanceiro.exceptions.ErroAutenticacao;
 import github.madeoliveira.controlefinanceiro.exceptions.RegraNegocioException;
 import github.madeoliveira.controlefinanceiro.model.entity.Usuario;
 import github.madeoliveira.controlefinanceiro.service.UsuarioService;
@@ -21,8 +22,17 @@ public class UsuarioResource {
 	public UsuarioResource(UsuarioService service) {
 		this.service = service;
 	}
+	@PostMapping("/autenticar")
+	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+		try {
+			Usuario usuarioAutenticacao = service.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(usuarioAutenticacao);
+		}catch(ErroAutenticacao e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 
-	@PostMapping
+	@PostMapping("/salvar")
 	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
 		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
 		try {
@@ -32,5 +42,6 @@ public class UsuarioResource {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
+	
 
 }
