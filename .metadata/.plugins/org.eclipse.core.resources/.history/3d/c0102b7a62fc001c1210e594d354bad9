5fc2a1c86e76c7d60f683bcdc9389d64bd1e12f8
Your Name <you@example.com>
2022-07-05 10:06:04 -0300

    Habilitado solicitações HTTP

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3d/c0102b7a62fc001c1210e594d354bad9
@@ -0,0 +1,13 @@
+package github.madeoliveira.controlefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlefinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlefinanceiroApplication.class, args);
+	}
+
+}
--- controlefinanceiro/src/main/java/github/madeoliveira/controlefinanceiro/ControlefinanceiroApplication.java
+++ controlefinanceiro/src/main/java/github/madeoliveira/controlefinanceiro/ControlefinanceiroApplication.java
@@ -2,9 +2,18 @@ package github.madeoliveira.controlefinanceiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ControlefinanceiroApplication {
+@EnableWebMvc
+public class ControlefinanceiroApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlefinanceiroApplication.class, args);
